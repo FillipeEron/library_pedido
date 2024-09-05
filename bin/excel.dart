25d990095185ab291c5cc9d0c678342d6a3f0ae9
FillipeEron <fillipeeron@gmail.com>
2024-09-05 00:28:36 -0300

    novos campos excel

--- bin/excel.dart
+++ bin/excel.dart
@@ -7,33 +7,92 @@ void main(List<String> args) {
   var propostaExcel = PropostaExcel();
   propostaExcel.id = "123";
   propostaExcel.numeroProposta = "4";
-  propostaExcel.date = "31/08/2028";
+  propostaExcel.data = "31/08/2028";
+  propostaExcel.dataProximoContato = "01/09/2028";
+  propostaExcel.idContato = "233";
+  propostaExcel.nomeContato = "2R ENGENHARIA E COMERCIO IMOBILIARIO LTDA";
+  propostaExcel.aosCuidados = "Fillipe Eron";
 
   propostaExcel.save();
-  /*var now = DateTime.now();
-  String date = now.toString().substring(0, 10);
-  String time = now.toString().substring(11, 19).replaceAll(':', '-');
-  String timeKey = "$date" + "_" + "$time";
-  print(timeKey);*/
-  /*
-  String file = '../docs/propostas.xlsx';
-  var bytes = File(file).readAsBytesSync();
-  var excel = Excel.decodeBytes(bytes);
-  Sheet sheetObject = excel["Propostas Comerciais"];
-  var cell = sheetObject.cell(CellIndex.indexByString("A1"));
-  print(cell.value);
-  String outputFile =
-      "/home/eron/Documentos/library_pedido/docs/propostas_2.xlsx";
-
-  List<int>? fileBytes = excel.save();
-  if (fileBytes != null) {
-    File(outputFile)
-      ..createSync(recursive: true)
-      ..writeAsBytesSync(fileBytes);
-  }*/
 }
 
 class PropostaExcel {
+  static const String file = "../docs/propostas.xlsx";
+  static const String pathOutputFile =
+      "/home/eron/Documentos/library_pedido/docs/";
+
+  var cellPosition = {
+    "id": "A2",
+    "numeroProposta": "B2",
+    "data": "C2",
+    "dataProximoContato": "D2",
+    "idContato": "E2",
+    "nomeContato": "F2",
+    "aosCuidados": "G2",
+    "listaDePreco": "H2",
+    "TipoPessoa": "I2",
+    "CPF/CNPJ": "J2",
+    "RG/IE": "K2",
+    "CEP": "L2",
+  };
+
+  String? id;
+  String? numeroProposta;
+  String? data;
+  String? dataProximoContato;
+  String? idContato;
+  String? nomeContato;
+  String? aosCuidados;
+  String? tipoPessoa;
+  String? cpf_cnpj;
+  String? rg_ie;
+  String? cep;
+
+  void save() {
+    String timeKey = this.timeKey();
+    var bytes = File(file).readAsBytesSync();
+    var excel = Excel.decodeBytes(bytes);
+    Sheet sheetObject = excel["Propostas Comerciais"];
+
+    var cellID = sheetObject.cell(CellIndex.indexByString(cellPosition["id"]!));
+    var cellNumeroProposta = sheetObject
+        .cell(CellIndex.indexByString(cellPosition["numeroProposta"]!));
+    var cellDate =
+        sheetObject.cell(CellIndex.indexByString(cellPosition["data"]!));
+    var cellDataProximoContato = sheetObject
+        .cell(CellIndex.indexByString(cellPosition["dataProximoContato"]!));
+    var cellIdContato =
+        sheetObject.cell(CellIndex.indexByString(cellPosition["idContato"]!));
+    var cellNomeContato =
+        sheetObject.cell(CellIndex.indexByString(cellPosition["nomeContato"]!));
+    var cellAosCuidados =
+        sheetObject.cell(CellIndex.indexByString(cellPosition["aosCuidados"]!));
+
+    cellID.value = TextCellValue(id ?? "");
+    cellNumeroProposta.value = TextCellValue(numeroProposta ?? "");
+    cellDate.value = TextCellValue(data ?? "");
+    cellDataProximoContato.value = TextCellValue(dataProximoContato ?? "");
+    cellIdContato.value = TextCellValue(idContato ?? "");
+    cellNomeContato.value = TextCellValue(nomeContato ?? "");
+    cellAosCuidados.value = TextCellValue(aosCuidados ?? "");
+
+    List<int>? fileBytes = excel.save();
+    if (fileBytes != null) {
+      File("$pathOutputFile/proposta_$timeKey.xlsx")
+        ..createSync(recursive: true)
+        ..writeAsBytesSync(fileBytes);
+    }
+  }
+
+  String timeKey() {
+    var now = DateTime.now();
+    String date = now.toString().substring(0, 10);
+    String time = now.toString().substring(11, 19).replaceAll(':', '-');
+    return "$date" + "_" + "$time";
+  }
+}
+
+/*class PropostaExcel {
   static const String file = "../docs/propostas.xlsx";
   static const String pathOutputFile =
       "/home/eron/Documentos/library_pedido/docs/";
@@ -50,6 +109,7 @@ class PropostaExcel {
     var bytes = File(file).readAsBytesSync();
     var excel = Excel.decodeBytes(bytes);
     Sheet sheetObject = excel["Propostas Comerciais"];
+    
     var cellID = sheetObject.cell(CellIndex.indexByString(positionID));
     var cellNumeroProposta =
         sheetObject.cell(CellIndex.indexByString(positionNumeroProposta));
@@ -73,4 +133,4 @@ class PropostaExcel {
     String time = now.toString().substring(11, 19).replaceAll(':', '-');
     return "$date" + "_" + "$time";
   }
-}
+}*/
