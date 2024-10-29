183a964acf4d5d0fe2feb16f0ec554488dc11ff3
FillipeEron <fillipeeron@gmail.com>
2024-10-28 22:30:22 -0300

    Ajuste no main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -9,43 +9,16 @@ import 'package:auxiliar_pedidos/proposta.dart';
 import 'package:auxiliar_pedidos/utils.dart';
 
 void main(List<String> arguments) async {
-  var visor = Visor(
-      moldura: Moldura.aluminio,
-      espessura: EspessuraVidro.e4,
-      largura: 400,
-      altura: 400);
+  var porta01 = PortaHDF(largura: 900, cor: CorHDF.curupixa);
 
-  var porta = PortaHDF(
-    largura: 830,
-    altura: 2240,
-    cor: CorHDF.branco,
-    desenho: Desenho.c01,
-    pintura: true,
-    mantaChumbo: true,
-    espessuraFolha: EspessuraFolha.e35,
-    identificacao: "P1",
-  );
+  var proposta = Proposta(
+      tabelaPreco: TabelaPreco.revenda,
+      data: dataAtual(),
+      vendedor: Vendedor.cleberson);
 
-  var porta2 = PortaHDF(
-    largura: 720,
-    altura: 2100,
-    cor: CorHDF.mogno,
-    espessuraFolha: EspessuraFolha.e35,
-    perfilU: true,
-    visor: visor,
-    identificacao: "P2",
-  );
-
-  var porta3 = PortaHDF(
-    largura: 700,
-    altura: 2100,
-    cor: CorHDF.imbuia,
-    espessuraFolha: EspessuraFolha.e45,
-    acustica: true,
-    identificacao: "P2",
-  );
-
-  var porta4 = PortaHDF(
+  await proposta.adicionarProduto(porta01, "2");
+  proposta.salvar();
+  /*var porta = PortaHDF(
     largura: 1000,
     altura: 2100,
     cor: CorHDF.curupixa,
@@ -61,9 +34,6 @@ void main(List<String> arguments) async {
       observacao: "proposta teste");
 
   await proposta.adicionarProduto(porta, "2");
-  await proposta.adicionarProduto(porta2, "1");
-  await proposta.adicionarProduto(porta3, "1");
-  await proposta.adicionarProduto(porta4, "1");
 
-  proposta.salvar();
+  proposta.salvar();*/
 }
